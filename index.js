0d6caf420a3b685f256ecc7603070b07845b20a5
Andre221 <kalaz.bb@yahoo.com>
2018-04-13 14:51:50 +1400

    Updated the readme, New feature templates

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var chalk = require("chalk")
 var d = new Date();
+
+var templates = {}
+
+function log(text, from) {
+    if (!text) throw "The first argument is missing."
+    if (typeof text !== 'object') throw "The first argument needs to be an object."
+    if (!from) {
+        from = process.title.split(' ')[5]
+    } else {
+        from = from.toUpperCase()
+    }
+
+    let date = chalk.gray(`${d.getFullYear()}-${d.getMonth()}-${d.getDate()}|${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}:${d.getMilliseconds()}`)
+    let de = chalk[text.color](text.text)
+
+    console.log(`[${date}]-[${from}] - ${de}`)
+}
+
 module.exports = {
     log(text, from) {
+        log(text, from)
+    },
+    addTemplate(name, text, from) {
         if (!text) throw "The first argument is missing."
         if (typeof text !== 'object') throw "The first argument needs to be an object."
         if (!from) {
@@ -10,10 +31,17 @@ module.exports = {
             from = from.toUpperCase()
         }
 
-        let date = chalk.gray(`${d.getFullYear()}-${d.getMonth()}-${d.getDate()}|${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}:${d.getMilliseconds()}`)
-        let de = chalk[text.color](text.text)
+        templates[name] = {
+            text: text,
+            from: from
+        }
 
-        console.log(`[${date}]-[${from}] - ${de}`)
+    },
+    loadFromTemplate(what) {
+        
+        var t = templates[what]
+
+        log(t.text, t.from)
 
     }
 }
--- readme.md
+++ readme.md
@@ -2,16 +2,19 @@ Loggic
 ============
 A module to help you log things from your application!
 
+To install, just do this  :
+```bash
+$ npm i loggic 
+```
+
 To use it in your application write this at the top of your app:
 ```js
-const {
-    log
-} = require('loggic')
+const loggic = require('loggic')
 ```
 
 This is an example
 ```js
-log({
+loggic.log({
     text: 'App started',
     color: 'green'
 }, 'test-app')
@@ -22,7 +25,7 @@ $ [2018-2-27|17-11-50:209]-[TEST-APP] - App started
 ```
 The second argument is optional so you can use loggic like this:
 ```js
-log({
+loggic.log({
     text: 'App started',
     color: 'green'
 })
@@ -30,4 +33,18 @@ log({
 Output:
 ```bash
 $ [2018-2-27|17-12-46:25]-[index.js] - App started
+```
+
+Also Loggic support templates, example : 
+```js
+loggic.addTemplate('test', {
+    text: 'App started',
+    color: 'red'
+}, 'test-app')
+``` 
+Will add the `test` template!
+
+Now to use you can do this  :
+```js
+loggic.loadFromTemplate('test')
 ```
